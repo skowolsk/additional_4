b3de0b40b5e2511fb8f0a1319c601332e2603c80
skowolsk <31890900+skowolsk@users.noreply.github.com>
2017-09-28 22:35:26 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+    let firstNum = first.split('').reverse();
+    let secondNum = second.split('').reverse();
+    let result = [];
+    result.length = firstNum.length + secondNum.length;
+
+    for(let i=0;i<result.length;i++)
+        result[i] = 0;
+
+    for (let i = 0; firstNum[i] >= 0; i++) {
+        for (let j = 0; secondNum[j] >= 0; j++) {
+            result[i + j] += firstNum[i] * secondNum[j];
+        }
+    }
+
+    let temp = 0;
+    for (let i = 0; i < result.length; i++) {
+        result[i] += temp;
+        temp = parseInt(result[i] / 10);
+        result[i] = result[i] % 10;
+    }
+
+    if( result[result.length-1] ===0){
+        result.pop();
+    }
+    return result.reverse().join('');
+};
